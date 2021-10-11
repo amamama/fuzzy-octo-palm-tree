2c6509bc3dc87a20722abde8684b6ebdf65ac948
memnarch <memnarch@ravni.ca>
2021-10-11 17:51:40 +0900

    tsurai

--- lean4.lean
+++ lean4.lean
@@ -7,3 +7,15 @@ by {
   injection h;
   assumption;
 }
+inductive Mem : α → List α → Prop where
+ | head (a : α) (as : List α)   : Mem a (a::as)
+ | tail (a b : α) (bs : List α) : Mem a bs → Mem a (b::bs)
+infix:50 "∈" => Mem
+theorem mem_split {a : α} {as : List α} (h : a ∈ as) : ∃ s t, as = s ++ a :: t := by
+  match a, as, h with
+  | _, _, Mem.head a bs     => exists []; exists bs; rfl
+  | _, _, Mem.tail a b bs h =>
+    match bs, mem_split h with
+    | _, ⟨s, ⟨t, rfl⟩⟩ =>
+      exists b::s; exists t;
+      rw [List.cons_append]
