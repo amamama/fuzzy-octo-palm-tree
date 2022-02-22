2de507c900b3bdf742379386652eb5e5de6d7cdb
memnarch <memnarch@ravni.ca>
2022-02-23 00:10:21 +0900

    nanika kaita

--- src/posnot.lean
+++ src/posnot.lean
@@ -1,28 +1,9 @@
-abbrev ℕ := Nat
-def PositionalNotation {n: ℕ} {h: n > 1} := List (Fin n)
-def PositionalNotation.toNat {n: ℕ} {h: n > 1}  :=
-  List.foldr (λ (a:Fin n) (b:Nat) => a + b * 10) 0
+import Lean
 
-theorem hodai0: ∀n m: Nat, n < m → 0 < m - n := by {
-  intro n;
-  induction n;
-  simp;
-  intros;
-  assumption;
-  case succ n ih => {
-    intro m;
-    induction m;
-    intro h1;
-    have h2 := Nat.not_lt_zero (Nat.succ n);
-    contradiction;
-    case succ m ih2 => {
-      simp [Nat.succ_sub_succ];
-      intro h2;
-      have h3 := Nat.lt_of_succ_lt_succ h2;
-      exact ih m h3;
-    }
-  }
-}
+abbrev ℕ := Nat
+abbrev PositionalNotation {b: {n: ℕ // n > 1}} := List (Fin b)
+def PositionalNotation.toNat {b: {n: ℕ // n > 1}} (pn: PositionalNotation) :=
+  List.foldr (λ (a:Fin b) (n:Nat) => a + n * b) 0 pn
 
 theorem hodai1: ∀x y:Nat, x ≥ 2 → y ≥ 2 → y ≤ x → x - y + 1 < x := by {
   intro x;
@@ -84,7 +65,7 @@ theorem hodai2: ∀n m: Nat,  n > 0 → m ≥ 2 → n / m < n := by {
     simp [pe, (Nat.not_le_of_gt pe), w];
     exact Nat.lt_of_succ_le h1;
     intro hh;
-    have hod := hodai0 y x hh;
+    have hod := Nat.zero_lt_sub_of_lt hh;
     specialize h3 hod h1;
     simp [h4, h2];
     have h5 := Nat.add_lt_add_right h3 1;
@@ -103,38 +84,166 @@ theorem hodai2: ∀n m: Nat,  n > 0 → m ≥ 2 → n / m < n := by {
   }
 }
 
-def toPositionalNotaion {base: ℕ} {h: base > 1} (n: ℕ) : @PositionalNotation base h :=
+theorem hodai3: ∀n: Nat, 0 / n = 0 := by {
+  intro n;
+  have h2 := Nat.div_eq 0 n;
+  cases n;
+  rw [h2];
+  simp;
+  case succ n => {
+    rw [h2];
+    have h3 := Nat.succ_pos n |> Nat.not_le_of_gt;
+    simp [h3];
+  }
+}
+
+theorem hodai4: ∀n: Nat, n > 0 → n / n = 1 := by {
+  intro n h1;
+  have h2 := Nat.div_eq n n;
+  simp [h1, Nat.le_refl] at h2;
+  simp [h2, Nat.sub_self, hodai3];
+}
+
+theorem hodai5: ∀n m k: Nat, m ≤ n → n + k - m = n - m + k := by {
+  intro n m k;
+  revert n m;
+  induction k;
+  simp;
+  case succ k ih1 => {
+    simp [Nat.add_succ];
+    intro n m;
+    revert n;
+    induction m;
+    simp;
+    case succ m ih2 => {
+      intro n;
+      induction n;
+      simp;
+      intro h1;
+      have h5 := Nat.eq_zero_of_le_zero h1;
+      contradiction;
+      case succ n ih3 => {
+        intro h1;
+        simp [Nat.succ_sub_succ];
+        have h2 := Nat.le_of_succ_le_succ h1;
+        have h3 := ih1 (Nat.succ n) m (Nat.le_step h2);
+        have h4 := ih2 n h2;
+        simp [←h4, Nat.succ_add];
+        
+      };
+    };
+  };
+}
+
+theorem hodai6: ∀n b, b > 1 → n % b < b := by {
+  intro n b h1;
+  apply Nat.mod_lt;
+  apply Nat.lt_of_le_and_ne;
+  exact Nat.zero_le b;
+  apply Ne.symm;
+  intro h3;
+  rw [h3] at h1;
+  contradiction;
+}
+
+theorem hodai7: ∀n m: Nat, n ≥ m → ¬m > n := by {
+  intro n m h1;
+  apply Or.elim $ Nat.eq_or_lt_of_le h1;
+  all_goals intro h2;
+  rw [h2];
+  exact Nat.lt_irrefl n;
+  intro h3;
+  exact absurd (Nat.lt_trans h2 h3) (Nat.lt_irrefl m);
+}
+
+theorem hodai8: ∀n m: Nat, n % m + n / m * m = n := by {
+  intro n m;
+  induction n, m using Nat.div.inductionOn;
+  case ind x y ih1 ih2 => {
+    have h1 := Nat.div_eq x y;
+    simp [h1, ih1, Nat.mod_eq_sub_mod, Nat.add_mul, ←Nat.add_assoc, ih2, ←hodai5, Nat.add_sub_self_right];
+  };
+  case base x y ih1 => {
+    have h1 := Nat.div_eq x y;
+    simp [h1, ih1];
+    have h2 := Nat.mod_eq x y;
+    simp [h2, ih1];
+  };
+}
+
+def toPositionalNotation {base: {n: ℕ // n > 1}} (n: ℕ) : @PositionalNotation base :=
   if h': n < base then 
     [Fin.mk n h']
-  else by {
-    have h1: base > 1 → n % base < base := by {
-      intro h2;
-      apply Nat.mod_lt;
-      apply Nat.lt_of_le_and_ne;
-      exact Nat.zero_le base;
-      apply Ne.symm;
-      intro h3;
-      rw [h3] at h2;
+  else 
+    have h1 := hodai6 n base base.property;
+    have h3: n / base < n := by {
+      apply hodai2;
+      apply Or.elim $ Nat.lt_or_ge n base;
+      intro;
       contradiction;
+      intro h3;
+      apply @Nat.lt_of_lt_of_le 0 base;
+      apply @Nat.lt_trans 0 1;
+      simp;
+      exact base.property;
+      assumption;
+      apply Nat.succ_le_of_lt;
+      exact base.property;
     };
-    exact Fin.mk (n % base) (h1 h) :: @toPositionalNotaion base h (n / base);
-  }
+    Fin.mk (n % base.val) h1 :: (toPositionalNotation (n / base))
   termination_by _ n => n
-  decreasing_by {
-    simp [invImage, InvImage, Prod.lex, sizeOfWFRel, measure, Nat.lt_wfRel, WellFoundedRelation.rel];
-    specialize h1 h;
-    apply hodai2;
-    have h2 := Nat.lt_or_ge n base;
-    apply Or.elim h2;
-    intro;
-    contradiction;
-    intro h3;
-    apply @Nat.lt_of_lt_of_le 0 base;
-    apply @Nat.lt_trans 0 1;
-    simp;
-    assumption;
-    assumption;
-    apply Nat.succ_le_of_lt;
-    assumption;
 
-  }
+theorem PositionalNotation.induction.F.{u} {b: { n: Nat // n > 1}}
+  (C : Nat → Sort u)
+  (ind: ∀n, n ≥ b.val → C (n / b) → C n)
+  (base: ∀n, ¬n ≥ b.val → C n)
+  (n: Nat) (f: ∀x', x' < n → C x') : C n :=
+  if h: n ≥ b then
+    have h1: n / b < n := by {
+      apply hodai2;
+      apply Or.elim $ Nat.lt_or_ge n b;
+      intro a;
+      exact absurd h (Nat.not_le_of_gt a);
+      intro;
+      apply @Nat.lt_of_lt_of_le 0 b;
+      apply @Nat.lt_trans 0 1;
+      simp;
+      exact b.property;
+      assumption;
+      apply Nat.succ_le_of_lt;
+      exact b.property;
+    };
+    ind n h (f (n / b) h1)
+  else base n h
+
+theorem PositionalNotation.inductionOn.{u}
+  (motive: Nat → Sort u)
+  (x: Nat)
+  (b: {n: Nat // n > 1}) -- koko hontou ha implicit ni sitai
+  (ind: ∀x, x ≥ b.val → motive (x / b) → motive x)
+  (base: ∀x, ¬x ≥ b.val → motive x)
+  : motive x :=
+  WellFounded.fix (measure id).wf (PositionalNotation.induction.F motive ind base) x
+
+theorem toNat_toPosNot_eq_Nat {b: {n: Nat // n > 1}} :∀n, (@toPositionalNotation b n).toNat = n := by {
+  intro n;
+  induction n using PositionalNotation.inductionOn;
+  assumption;
+  case ind n ih1 ih2 => {
+    unfold toPositionalNotation;
+    apply Or.elim $ Nat.lt_or_ge n b;
+    intro h2;
+    simp [h2, PositionalNotation.toNat, List.foldr];
+    intro h2;
+    have h3 := hodai7 n b h2;
+    simp [PositionalNotation.toNat] at ih2;
+    simp [h3, PositionalNotation.toNat, List.foldr, ih2];
+  };
+}
+
+variable (α : Sort u)
+variable (r : α → α → Prop)
+
+#check (Acc r : α → Prop)
+#check @WellFounded.fix
+#check 
